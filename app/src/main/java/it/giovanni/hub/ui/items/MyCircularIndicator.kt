20e3c2ae86d98b336266a3b68b5b73003c0bf9f5
giovanni.petito <giovanni.petito@finconsgroup.com>
2023-10-06 17:37:53 +0200

    create CircularIndicator function

---
+++ app/src/main/java/it/giovanni/hub/ui/items/MyCircularIndicator.kt
@@ -0,0 +1,61 @@
+package it.giovanni.hub.ui.items
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.DefaultAlpha
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CircularIndicator(
+    canvasSize: Dp = 300.dp,
+    indicatorValue: Int = 0,
+    maxIndicatorValue: Int = 100,
+    backgroundIndicatorColor: Color = MaterialTheme.colorScheme.surface.copy(alpha = DefaultAlpha),
+    backgroundIndicatorStrokeWidth: Float = 100f
+) {
+    Column(modifier = Modifier
+        .size(canvasSize)
+        .drawBehind {
+            val componentSize = size / 1.25f
+            backgroundIndicator(
+                componentSize = componentSize,
+                indicatorColor = backgroundIndicatorColor,
+                indicatorStrokeWidth = backgroundIndicatorStrokeWidth
+            )
+        }) {
+    }
+}
+
+fun DrawScope.backgroundIndicator(
+    componentSize: Size,
+    indicatorColor: Color,
+    indicatorStrokeWidth: Float
+) {
+    drawArc(
+        color = indicatorColor,
+        startAngle = 150f,
+        sweepAngle = 240f,
+        useCenter = false,
+        style = Stroke(
+            width = indicatorStrokeWidth,
+            cap = StrokeCap.Round
+        )
+    )
+}
+
+@Preview
+@Composable
+fun CircularIndicatorPreview(
+) {
+}
