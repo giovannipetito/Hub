a67fa4b1a3503373295322fb338da67cfd618b6c
giovanni.petito <giovanni.petito@finconsgroup.com>
2023-10-13 01:12:05 +0200

    CircularIndicator implementation completed

--- app/src/main/java/it/giovanni/hub/screens/details/UIScreen.kt
+++ app/src/main/java/it/giovanni/hub/screens/details/UIScreen.kt
@@ -7,14 +7,20 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,6 +29,7 @@ import androidx.navigation.compose.rememberNavController
 import it.giovanni.hub.Constants
 import it.giovanni.hub.MainActivity
 import it.giovanni.hub.ui.items.Card2
+import it.giovanni.hub.ui.items.CircularIndicator
 import it.giovanni.hub.ui.items.rainbowColors
 
 @Composable
@@ -59,6 +66,25 @@ fun UIScreen(navController: NavController, mainActivity: MainActivity) {
                 verticalArrangement = Arrangement.SpaceEvenly
             ) {
                 Card2(title = "Expandable Card")
+
+                var value by remember {
+                    mutableStateOf(0)
+                }
+                CircularIndicator(
+                    indicatorValue = value
+                )
+
+                TextField(
+                    value = value.toString(),
+                    onValueChange = { input ->
+                            value = if (input.isNotEmpty()) {
+                                input.toInt()
+                            } else {
+                                0
+                            }
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                )
             }
         }
     }
--- app/src/main/java/it/giovanni/hub/ui/items/MyCircularIndicator.kt
+++ app/src/main/java/it/giovanni/hub/ui/items/MyCircularIndicator.kt
@@ -1,19 +1,34 @@
 package it.giovanni.hub.ui.items
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.DefaultAlpha
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -21,9 +36,52 @@ fun CircularIndicator(
     canvasSize: Dp = 300.dp,
     indicatorValue: Int = 0,
     maxIndicatorValue: Int = 100,
-    backgroundIndicatorColor: Color = MaterialTheme.colorScheme.surface.copy(alpha = DefaultAlpha),
-    backgroundIndicatorStrokeWidth: Float = 100f
+    backgroundIndicatorColor: Color = MaterialTheme.colorScheme.primaryContainer,
+    backgroundIndicatorStrokeWidth: Float = 100f,
+    foregroundIndicatorColor: Color = MaterialTheme.colorScheme.primary,
+    foregroundIndicatorStrokeWidth: Float = 100f,
+    bigTextColor: Color = MaterialTheme.colorScheme.primary,
+    bigTextSuffix: String = "GB",
+    bigTextFontSize: TextUnit = MaterialTheme.typography.displayLarge.fontSize,
+    smallText: String = "Remaining",
+    smallTextColor: Color = MaterialTheme.colorScheme.primaryContainer,
+    smallTextFontSize: TextUnit = MaterialTheme.typography.displaySmall.fontSize
 ) {
+
+    var allowedIndicatorValue by remember {
+        mutableStateOf(maxIndicatorValue)
+    }
+    allowedIndicatorValue = if (indicatorValue <= maxIndicatorValue) {
+        indicatorValue
+    } else {
+        maxIndicatorValue
+    }
+
+    var animatedIndicatorValue by remember { mutableStateOf(0f) }
+    LaunchedEffect(key1 = allowedIndicatorValue) {
+        animatedIndicatorValue = allowedIndicatorValue.toFloat()
+    }
+
+    val percentage = (animatedIndicatorValue / maxIndicatorValue) * 100
+
+    val sweepAngle by animateFloatAsState(
+        targetValue = (2.4 * percentage).toFloat(),
+        animationSpec = tween(1000)
+    )
+
+    val receivedValue by animateIntAsState(
+        targetValue = allowedIndicatorValue,
+        animationSpec = tween(1000)
+    )
+
+    val animatedBigTextColor by animateColorAsState(
+        targetValue = if (allowedIndicatorValue == 0)
+            MaterialTheme.colorScheme.primaryContainer
+        else
+            bigTextColor,
+        animationSpec = tween(1000)
+    )
+
     Column(modifier = Modifier
         .size(canvasSize)
         .drawBehind {
@@ -33,7 +91,25 @@ fun CircularIndicator(
                 indicatorColor = backgroundIndicatorColor,
                 indicatorStrokeWidth = backgroundIndicatorStrokeWidth
             )
-        }) {
+            foregroundIndicator(
+                sweepAngle = sweepAngle,
+                componentSize = componentSize,
+                indicatorColor = foregroundIndicatorColor,
+                indicatorStrokeWidth = foregroundIndicatorStrokeWidth
+            )
+        },
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        EmbeddedElements(
+            bigText = receivedValue,
+            bigTextColor = animatedBigTextColor,
+            bigTextSuffix = bigTextSuffix,
+            bigTextFontSize = bigTextFontSize,
+            smallText = smallText,
+            smallTextColor = smallTextColor,
+            smallTextFontSize = smallTextFontSize
+        )
     }
 }
 
@@ -43,6 +119,7 @@ fun DrawScope.backgroundIndicator(
     indicatorStrokeWidth: Float
 ) {
     drawArc(
+        size = componentSize,
         color = indicatorColor,
         startAngle = 150f,
         sweepAngle = 240f,
@@ -50,12 +127,65 @@ fun DrawScope.backgroundIndicator(
         style = Stroke(
             width = indicatorStrokeWidth,
             cap = StrokeCap.Round
+        ),
+        topLeft = Offset(
+            x = (size.width - componentSize.width) / 2f,
+            y = (size.height - componentSize.height) / 2f
         )
     )
 }
 
-@Preview
+fun DrawScope.foregroundIndicator(
+    sweepAngle: Float,
+    componentSize: Size,
+    indicatorColor: Color,
+    indicatorStrokeWidth: Float
+) {
+    drawArc(
+        size = componentSize,
+        color = indicatorColor,
+        startAngle = 150f,
+        sweepAngle = sweepAngle,
+        useCenter = false,
+        style = Stroke(
+            width = indicatorStrokeWidth,
+            cap = StrokeCap.Round
+        ),
+        topLeft = Offset(
+            x = (size.width - componentSize.width) / 2f,
+            y = (size.height - componentSize.height) / 2f
+        )
+    )
+}
+
 @Composable
+fun EmbeddedElements(
+    bigText: Int,
+    bigTextColor: Color,
+    bigTextSuffix: String,
+    bigTextFontSize: TextUnit,
+    smallText: String,
+    smallTextColor: Color,
+    smallTextFontSize: TextUnit,
+) {
+    Text(
+        text = smallText,
+        color = smallTextColor,
+        fontSize = smallTextFontSize,
+        textAlign = TextAlign.Center
+    )
+    Text(
+        text = "$bigText ${bigTextSuffix.take(2)}",
+        color = bigTextColor,
+        fontSize = bigTextFontSize,
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+@Preview(showBackground = true)
 fun CircularIndicatorPreview(
 ) {
+    CircularIndicator()
 }
