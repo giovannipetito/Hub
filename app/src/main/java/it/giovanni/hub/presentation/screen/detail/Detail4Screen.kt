18154798718dcdfea0ba3c9bb7939f4d4c27b895
Giovanni <giovanni.petito88@gmail.com>
2023-11-07 00:32:42 +0100

    implements mutableStateListOf

--- app/src/main/java/it/giovanni/hub/presentation/screen/detail/Detail4Screen.kt
+++ app/src/main/java/it/giovanni/hub/presentation/screen/detail/Detail4Screen.kt
@@ -3,8 +3,16 @@ package it.giovanni.hub.presentation.screen.detail
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -36,21 +45,55 @@ fun Detail4Screen(
         Log.i("[Person]", person.value?.firstName + " " + person.value?.lastName)
     }
 
+    val list = personViewModel.list
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background),
         contentAlignment = Alignment.Center,
     ) {
-        Text(
-            modifier = Modifier.clickable {
-                navController.popBackStack()
-            },
-            text = "$firstName $lastName",
-            color = Color.Red,
-            fontSize = 40.sp,
-            fontWeight = FontWeight.Bold
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(state = rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Text(
+                modifier = Modifier.clickable {
+                    navController.popBackStack()
+                },
+                text = "$firstName $lastName",
+                color = Color.Red,
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.White
+                ),
+                onClick = {
+                    personViewModel.addRandomPerson()
+                }
+            ) {
+                Text(
+                    text = "Add Random Person",
+                    color = Color.Red
+                )
+            }
+            list.forEach { person ->
+                Text(
+                    text = person.firstName + " " + person.lastName,
+                    color = Color.Blue,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
     }
 }
 
--- app/src/main/java/it/giovanni/hub/presentation/viewmodel/PersonViewModel.kt
+++ app/src/main/java/it/giovanni/hub/presentation/viewmodel/PersonViewModel.kt
@@ -1,10 +1,13 @@
 package it.giovanni.hub.presentation.viewmodel
 
+import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import it.giovanni.hub.data.model.Person
+import kotlin.random.Random
 
 class PersonViewModel : ViewModel() {
 
@@ -13,5 +16,30 @@ class PersonViewModel : ViewModel() {
 
     fun addPerson(newPerson: Person?) {
         _person.value = newPerson
+    }
+
+    private val randomList: List<Person> = listOf(
+        Person("Giovanni", "Petito"),
+        Person("Tara", "Tandel"),
+        Person("Angelina", "Basile"),
+        Person("Vincenzo", "Petito"),
+        Person("Raffaele", "Petito"),
+        Person("Teresa", "Petito"),
+        Person("Salvatore", "Pragliola"),
+        Person("Ilenia", "Pragliola"),
+        Person("Armando", "Pragliola")
+    )
+
+    // mutableStateListOf è observable e il suo utilizzo ci permette di fare il compose dello
+    // screen con successo
+    private var _list = mutableStateListOf<Person>()
+    val list: List<Person> = _list
+
+    fun addRandomPerson() {
+        val randomPerson: Person = randomList.random()
+        _list.add(randomPerson)
+
+        val randomId = Random.nextInt(from = 1, until = 100)
+        Log.i("[Person]", "" + randomId + ") " + _list.last().firstName + " " + _list.last().lastName)
     }
 }
