e126c320346b9abf417ade78b5319e91d18e334c
Giovanni <giovanni.petito88@gmail.com>
2023-12-04 01:14:27 +0100

    fix BottomAppBar navigation

--- app/src/main/java/it/giovanni/hub/navigation/util/set/BottomAppBarSet.kt
+++ app/src/main/java/it/giovanni/hub/navigation/util/set/BottomAppBarSet.kt
@@ -1,7 +1,29 @@
 package it.giovanni.hub.navigation.util.set
 
-sealed class BottomAppBarSet(val route: String) {
-    data object Home: BottomAppBarSet(route = "home_screen")
-    data object Profile: BottomAppBarSet(route = "profile_screen")
-    data object Settings: BottomAppBarSet(route = "settings_screen")
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
+enum class BottomAppBarSet(
+    val route: String,
+    val contentDescription: String,
+    val icon: ImageVector
+) {
+    Home(
+        route = "home_screen",
+        contentDescription = "Home",
+        icon = Icons.Default.Home
+    ),
+    Profile(
+        route = "profile_screen",
+        contentDescription = "Profile",
+        icon = Icons.Default.Person
+    ),
+    Settings(
+        route = "settings_screen",
+        contentDescription = "Settings",
+        icon = Icons.Default.Settings
+    )
 }
--- app/src/main/java/it/giovanni/hub/ui/items/HubBottomAppBar.kt
+++ app/src/main/java/it/giovanni/hub/ui/items/HubBottomAppBar.kt
@@ -1,58 +1,78 @@
 package it.giovanni.hub.ui.items
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import it.giovanni.hub.navigation.util.set.BottomAppBarSet
 
 @Composable
 fun HubBottomAppBar(navController: NavHostController) {
-    BottomAppBar(
-        actions = {
-            IconButton(onClick = {
-                navController.navigate(route = BottomAppBarSet.Home.route) {
-                    popUpTo(navController.graph.findStartDestination().id)
-                    launchSingleTop = true
+
+    val currentRoute: String? = getCurrentRoute(navController)
+    var itemColor: Color = MaterialTheme.colorScheme.primary
+
+    if (BottomAppBarSet.entries.any { it.route == currentRoute }) {
+        BottomAppBar(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = itemColor,
+            // contentPadding = PaddingValues(start = 24.dp, end = 24.dp),
+            actions = {
+                BottomAppBarSet.entries.forEach { screen ->
+
+                    val isSelected = screen.route == currentRoute
+                    itemColor = if (isSelected)
+                        MaterialTheme.colorScheme.primary
+                    else
+                        MaterialTheme.colorScheme.primary.copy(alpha = 0.6f)
+
+                    IconButton(
+                        modifier = Modifier.weight(weight = 1f),
+                        onClick = {
+                            navController.navigate(route = screen.route) {
+                                popUpTo(navController.graph.findStartDestination().id)
+                                launchSingleTop = true
+                            }
+                        }
+                    ) {
+                        Icon(
+                            screen.icon,
+                            contentDescription = screen.contentDescription,
+                            tint = itemColor,
+                            modifier = Modifier.size(36.dp)
+                        )
+                    }
                 }
-            }) {
-                Icon(Icons.Filled.Home, contentDescription = "Home")
-            }
-            IconButton(onClick = {
-                navController.navigate(route = BottomAppBarSet.Profile.route) {
-                    popUpTo(navController.graph.findStartDestination().id)
-                    launchSingleTop = true
+            },
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = { /* do something */ },
+                    containerColor = MaterialTheme.colorScheme.tertiaryContainer, // BottomAppBarDefaults.bottomAppBarFabColor,
+                    contentColor = MaterialTheme.colorScheme.tertiary,
+                    elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+                ) {
+                    Icon(Icons.Filled.Add, "Localized description")
                 }
-            }) {
-                Icon(Icons.Filled.Person, contentDescription = "Profile")
             }
-            IconButton(onClick = {
-                navController.navigate(route = BottomAppBarSet.Settings.route) {
-                    popUpTo(navController.graph.findStartDestination().id)
-                    launchSingleTop = true
-                }
-            }) {
-                Icon(Icons.Filled.Settings, contentDescription = "Settings")
-            }
-        },
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = { /* do something */ },
-                containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
-                elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
-            ) {
-                Icon(Icons.Filled.Add, "Localized description")
-            }
-        }
-    )
+        )
+    }
+}
+
+@Composable
+fun getCurrentRoute(navController: NavHostController): String? {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    return navBackStackEntry?.destination?.route
 }
