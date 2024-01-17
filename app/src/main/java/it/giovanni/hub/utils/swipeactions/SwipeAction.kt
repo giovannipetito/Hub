1f8132fe8e5870603d3026de419416876ede19f5
Giovanni <giovanni.petito88@gmail.com>
2024-01-17 03:30:27 +0100

    remove swipe weight

--- app/src/main/java/it/giovanni/hub/utils/swipeactions/SwipeAction.kt
+++ app/src/main/java/it/giovanni/hub/utils/swipeactions/SwipeAction.kt
@@ -4,20 +4,11 @@ import androidx.compose.runtime.Composable
 
 /**
  * Represents an action that can be shown in [SwipeActionsBox].
- *
- * @param weight The proportional width to give to this element, as related to the total of
- * all weighted siblings. [SwipeActionsBox] will divide its horizontal space and distribute
- * it to actions according to their weight.
  */
 class SwipeAction(
     val onSwipe: (String) -> Unit,
-    val weight: Double = 1.0,
     val icon: @Composable () -> Unit,
-) {
-    init {
-        require(weight > 0.0) { "invalid weight $weight; must be greater than zero." }
-    }
-}
+)
 
 data class SwipedAction(
     val swipeActions: List<SwipeAction>,
