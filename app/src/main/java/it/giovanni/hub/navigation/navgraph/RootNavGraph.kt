bc3168ffcdbef62c8285d22fccd7ad39cf4d4551
Giovanni <giovanni.petito88@gmail.com>
2024-01-27 02:50:43 +0100

    implement NewMainNavGraph

--- app/src/main/java/it/giovanni/hub/navigation/navgraph/RootNavGraph.kt
+++ app/src/main/java/it/giovanni/hub/navigation/navgraph/RootNavGraph.kt
@@ -5,13 +5,10 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import it.giovanni.hub.domain.service.CounterService
-import it.giovanni.hub.navigation.Graph.MAIN_ROUTE
 import it.giovanni.hub.navigation.Graph.WIZARD_ROUTE
 import it.giovanni.hub.navigation.Graph.ROOT_ROUTE
 import it.giovanni.hub.navigation.Graph.LOADING_ROUTE
-import it.giovanni.hub.presentation.screen.main.MainScreen
 import it.giovanni.hub.presentation.screen.main.LoadingScreen
 import it.giovanni.hub.presentation.screen.main.WizardScreen
 import it.giovanni.hub.presentation.viewmodel.MainViewModel
@@ -45,16 +42,15 @@ fun RootNavGraph(
             WizardScreen(navController = navController)
         }
         loginNavGraph(navController = navController, mainViewModel = mainViewModel)
-        composable(route = MAIN_ROUTE) {
-            MainScreen(
-                darkTheme = darkTheme,
-                dynamicColor = dynamicColor,
-                onThemeUpdated = onThemeUpdated,
-                onColorUpdated = onColorUpdated,
-                navController = rememberNavController(),
-                mainViewModel = mainViewModel,
-                counterService = counterService
-            )
-        }
+
+        mainScreenNavGraph(
+            darkTheme = darkTheme,
+            dynamicColor = dynamicColor,
+            onThemeUpdated = onThemeUpdated,
+            onColorUpdated = onColorUpdated,
+            navController = navController,
+            mainViewModel = mainViewModel,
+            counterService = counterService
+        )
     }
 }
